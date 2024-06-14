1f39f0835cc7fbe445d1e37f045f5b67670ea619
guilhermecazine <guilhermecazine50@gmail.com>
2024-06-14 01:28:55 +0100

    Basic JavaScript Projects Assigment

--- Basic Javascript Projects/Project1_expressions_alert/JS/main.js
+++ Basic Javascript Projects/Project1_expressions_alert/JS/main.js
@@ -2,8 +2,9 @@
 document.write("Hello, World!");
 
 // Create and assign values to variables
-var A = "Snow", B = "Lupi", C = "Theo", D = "Nina";
-window.alert(A + B);
+var Sent1 = "This is the beginning of the string ", Sent2 = "and this is the end of the string";
+
+window.alert(Sent1 + Sent2);
 
 // Prints out an expression
 document.write(3 + 3);
