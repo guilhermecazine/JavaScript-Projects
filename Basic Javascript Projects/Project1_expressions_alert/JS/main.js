2f0dc3787f0bef92d6918daa2321c46e286b3ccf
guilhermecazine <guilhermecazine50@gmail.com>
2024-06-14 01:25:39 +0100

    first commit

---
+++ Basic Javascript Projects/Project1_expressions_alert/JS/main.js
@@ -0,0 +1,9 @@
+// Writes "Hello, World!"
+document.write("Hello, World!");
+
+// Create and assign values to variables
+var A = "Snow", B = "Lupi", C = "Theo", D = "Nina";
+window.alert(A + B);
+
+// Prints out an expression
+document.write(3 + 3);
